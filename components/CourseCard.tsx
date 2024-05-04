b435400a94e4d388b0a89d3a9417780bdee92d27
Netra <netrahindocha1@gmail.com>
2024-05-04 06:43:47 -0400

    Login and profile layout designed

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -1,6 +1,17 @@
-import Image from "next/image"
+"use client"
+
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 const CourseCard = () => {
+
+    const [addCourse, setAddCourse] = useState();
+
+    const add = () => {
+
+    }
+
     return (
         <div>
             <div className="flex gap-5 w-[300px] mx-auto">
